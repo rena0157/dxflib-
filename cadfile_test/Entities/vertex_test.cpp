61362e9b8a4fe490363da4bbc966f170542517c6
rena0157 <33334607+rena0157@users.noreply.github.com>
2018-04-29 10:06:02 -0400

    added within arc test

--- cadfile_test/Entities/vertex_test.cpp
+++ cadfile_test/Entities/vertex_test.cpp
@@ -16,7 +16,7 @@ namespace entity_tests
 			Assert::AreEqual(3.1, test_vertex.z, 0.1);
 		}
 
-		TEST_METHOD(within_geoline)
+		TEST_METHOD(within)
 		{
 			dxflib::cadfile c_file{ R"(C:\Dev\Test_DXF_files\vertex\within.dxf)" };
 			
@@ -24,6 +24,20 @@ namespace entity_tests
 			dxflib::entities::vertex vertex{ 4, 3.3 };
 			Assert::IsTrue(vertex.within(c_file.get_lwpolylines()[0]));
 
+			// with geoline
+			Assert::IsTrue(vertex.within(c_file.get_lwpolylines()[0].get_lines()));
+		}
+
+		TEST_METHOD(within_arc)
+		{
+			dxflib::cadfile c_file{ R"(C:\Dev\Test_DXF_files\vertex\within_arc.dxf)" };
+
+			// With polyline - fails because the vertex is within an arc segment
+			dxflib::entities::vertex vertex{ 2.5, 3.7 };
+			Assert::IsTrue(vertex.within(c_file.get_lwpolylines()[0]), L"pl failure");
+
+			// with geoline
+			Assert::IsTrue(vertex.within(c_file.get_lwpolylines()[0].get_lines()), L"geoline failure");
 		}
 	};
 }
