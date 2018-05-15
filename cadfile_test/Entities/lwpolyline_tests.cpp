e6673a24305fbe759056c0d0aaa7825b5c92198f
rena0157 <33334607+rena0157@users.noreply.github.com>
2018-05-15 19:45:18 -0400

    Added a within with arcs test and renamed the existing test to without arcs

--- cadfile_test/Entities/lwpolyline_tests.cpp
+++ cadfile_test/Entities/lwpolyline_tests.cpp
@@ -5,13 +5,23 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 TEST_CLASS(lwpolyline)
 {
-	TEST_METHOD(within_test)
+	TEST_METHOD(within_test_non_arced)
 	{
-		dxflib::cadfile c_file{ R"(C:\Dev\Test_DXF_files\lwpolyline\within_test.dxf)" };
+		dxflib::cadfile c_file{ R"(C:\Dev\Test_DXF_files\lwpolyline\within_test_non_arced.dxf)" };
 
 		for (const auto& pl : c_file.get_lwpolylines())
 			for (const auto& t : c_file.get_text())
 				Assert::IsTrue(t.first_alignment().within(pl),
 					std::wstring{t.get_string().begin(), t.get_string().end()}.c_str());
+	}
+
+	TEST_METHOD(within_test_arced)
+	{
+		dxflib::cadfile c_file{ R"(C:\Dev\Test_DXF_files\lwpolyline\within_test_arced.dxf)" };
+
+		for (const auto& pl : c_file.get_lwpolylines())
+			for (const auto& t : c_file.get_text())
+				Assert::IsTrue(t.first_alignment().within(pl),
+					std::wstring{ t.get_string().begin(), t.get_string().end() }.c_str());
 	}
 };
